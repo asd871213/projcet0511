d59405d69d15f4a4b67f0029d4035627c23c23c0
peihanchou <asd871213@gmail.com>
2018-05-18 10:29:33 +0800

    pro0511

--- project.c
+++ project.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int summary(int init,int endpoint);
+int Summary(int init,int endpoint);
 
 int main(int argc, char *argv[]) {
-printf("summary=%d\n",summary(1,10));
+printf("Summary=%d\n",Summary(1,10));
+
 	return 0;
 }
--- summary.c
+++ summary.c
@@ -1,7 +1,7 @@
-int summary(){
+int Summary(){
 
-	int init,endpoint=10,summary;
-	for(init=0,summary=0;init<endpoint;init++)
-		summary=summary+init;
- return summary;		
+	int init,endpoint=10,Summary;
+	for(init=0,Summary=0;init<endpoint;init++)
+		Summary=Summary+init;
+ return Summary;		
 }	
